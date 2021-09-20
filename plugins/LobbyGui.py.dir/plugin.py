d5ad26167216b366a472337f5e6fc3c9ed6e56aa
Jan Helbling <jh@jan-helbling.ch>
2021-09-20 17:24:03 +0200

    cleanedup Code

--- plugins/LobbyGui.py.dir/plugin.py
+++ plugins/LobbyGui.py.dir/plugin.py
@@ -9,8 +9,8 @@ class MeinJoinEvent(PythonListener):
     @PythonEventHandler(PlayerJoinEvent, EventPriority.NORMAL)  # Hier kommt das Event
     def onEvent(self, event):
         # loc = event.getSpawnLocation()  # location
-        l = event.getPlayer().getWorld().getName()  # weltname
-        if l.decode() == 'Lobby':
+        weltname = event.getPlayer().getWorld().getName().decode()  # weltname
+        if weltname == 'Lobby':
             event.getPlayer().getInventory().addItem(ItemStack(bukkit.Material.COMPASS, 1))
             #event.getPlayer().setCompassTarget(location)
 
@@ -19,8 +19,8 @@ class MeinInteractEvent(PythonListener):
     def onEvent(self, event):
         if event.getItem().getType() == bukkit.Material.COMPASS:
             # loc = event.getSpawnLocation()  # location
-            l = event.getPlayer().getWorld().getName()  # weltname
-            if l.decode() == 'Lobby':
+            weltname = event.getPlayer().getWorld().getName().decode()  # weltname
+            if weltname == 'Lobby':
                 inv = event.getPlayer().getServer().createInventory(event.getPlayer(), 27, "KOMPASS")
                 test = ItemStack(bukkit.Material.DIAMOND, 1)
                 testmeta = test.getItemMeta()
@@ -33,11 +33,13 @@ class MeinInteractEvent(PythonListener):
 class MeinInventoryEvent(PythonListener):
         @PythonEventHandler(InventoryClickEvent, EventPriority.NORMAL)
         def onEvent(self, event):
-            if event.getView().getPlayer().getWorld().getName().decode() == 'Lobby':
+            weltname = event.getView().getPlayer().getWorld().getName().decode()
+            if weltname == 'Lobby':
     	        item = event.getCurrentItem()
 	        if item.getType() == bukkit.Material.DIAMOND:
 	            event.setCancelled(True)
 	            event.getView().getPlayer().sendMessage("OPTION #1 GEKLICKT!")
+	            #event.getPlayer().setCompassTarget(location)
 
 class LobbyGUI(PythonPlugin):
     def onEnable(self):
