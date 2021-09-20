39e39714e59a6df53aeac7631b4521ba912ab546
Jan Helbling <jh@jan-helbling.ch>
2021-09-20 17:56:51 +0200

    now it works

--- plugins/LobbyGui.py.dir/plugin.py
+++ plugins/LobbyGui.py.dir/plugin.py
@@ -2,44 +2,49 @@
 
 from org.bukkit.event import EventPriority
 from org.bukkit.inventory import ItemStack
-from org.bukkit.event.player import PlayerJoinEvent,PlayerInteractEvent
+from org.bukkit.event.player import PlayerJoinEvent, PlayerInteractEvent
 from org.bukkit.event.inventory import InventoryClickEvent
 
+
 class MeinJoinEvent(PythonListener):
-    @PythonEventHandler(PlayerJoinEvent, EventPriority.NORMAL)  # Hier kommt das Event
+    @PythonEventHandler(PlayerJoinEvent,
+                        EventPriority.NORMAL)  # Hier kommt das Event
     def onEvent(self, event):
         # loc = event.getSpawnLocation()  # location
-        weltname = event.getPlayer().getWorld().getName().decode()  # weltname
-        if weltname == 'Lobby':
+        l = event.getPlayer().getWorld().getName()  # weltname
+        if l.decode() == 'Lobby':
             event.getPlayer().getInventory().addItem(ItemStack(bukkit.Material.COMPASS, 1))
-            #event.getPlayer().setCompassTarget(location)
+            # event.getPlayer().setCompassTarget(location)
+
 
 class MeinInteractEvent(PythonListener):
-    @PythonEventHandler(PlayerInteractEvent, EventPriority.NORMAL)  # Hier kommt das Event
+    @PythonEventHandler(PlayerInteractEvent,
+                        EventPriority.NORMAL)  # Hier kommt das Event
     def onEvent(self, event):
         if event.getItem().getType() == bukkit.Material.COMPASS:
             # loc = event.getSpawnLocation()  # location
-            weltname = event.getPlayer().getWorld().getName().decode()  # weltname
-            if weltname == 'Lobby':
-                inv = event.getPlayer().getServer().createInventory(event.getPlayer(), 27, "KOMPASS")
+            l = event.getPlayer().getWorld().getName()  # weltname
+            if l.decode() == 'Lobby':
+                inv = event.getPlayer().getServer().createInventory(
+                    event.getPlayer(), 27, "KOMPASS")
                 test = ItemStack(bukkit.Material.DIAMOND, 1)
                 testmeta = test.getItemMeta()
                 testmeta.setDisplayName("Menu Eintrag #1")
                 test.setItemMeta(testmeta)
                 inv.addItem(test)
                 event.getPlayer().openInventory(inv)
-                #event.getPlayer().setCompassTarget(location)
+                # event.getPlayer().setCompassTarget(location)
+
 
 class MeinInventoryEvent(PythonListener):
-        @PythonEventHandler(InventoryClickEvent, EventPriority.NORMAL)
-        def onEvent(self, event):
-            weltname = event.getView().getPlayer().getWorld().getName().decode()
-            if weltname == 'Lobby':
-    	        item = event.getCurrentItem()
-	        if item.getType() == bukkit.Material.DIAMOND:
-	            event.setCancelled(True)
-	            event.getView().getPlayer().sendMessage("OPTION #1 GEKLICKT!")
-	            #event.getPlayer().setCompassTarget(location)
+    @PythonEventHandler(InventoryClickEvent, EventPriority.NORMAL)
+    def onEvent(self, event):
+        if event.getView().getPlayer().getWorld().getName().decode() == 'Lobby':
+            item = event.getCurrentItem()
+            if item.getType() == bukkit.Material.DIAMOND:
+                event.setCancelled(True)
+                event.getView().getPlayer().sendMessage("OPTION #1 GEKLICKT!")
+
 
 class LobbyGUI(PythonPlugin):
     def onEnable(self):
@@ -51,5 +56,6 @@ class LobbyGUI(PythonPlugin):
         self.pm.registerEvents(self.lst1, self)
         self.pm.registerEvents(self.lst2, self)
         self.pm.registerEvents(self.lst3, self)
+
     def onDisable(self):
         self.getLogger().info("Lobby-GUI disabled!")
