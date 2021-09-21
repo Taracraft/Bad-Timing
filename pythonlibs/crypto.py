c8d0aeb3b3b77a54cfee031bf5ccfc32d26ab6c8
Jan Helbling <jh@jan-helbling.ch>
2021-09-22 00:53:30 +0200

    API um den aktuellen Preis der Kryptowaehrungen abzufragen

---
+++ pythonlibs/crypto.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import urllib.request,urllib.parse,urllib.error
+import json
+
+API_KEY = "58f662b3d9028fe8ca2e52a3682d8390c5b02915f991d0d14f73fcec36e34228"
+API_CUR = "USD"
+API_URL = "https://min-api.cryptocompare.com/data/pricemulti"
+API_CCUR= "BTC,LTC,ETH,DOGE,IOTA"
+
+API_FULL_URL = API_URL + "?fsyms=" + API_CCUR + "&tsyms=" + API_CUR
+
+class getLiveData(object):
+	def __init__(self):
+		self.__opener__ = urllib.request.build_opener()
+		self.__opener__.addheaders = [('User-Agent','BadTiming MC Server')]
+		self.__get_all__()
+	
+	def __get_all__(self):
+		self.__json__ = json.loads(self.__opener__.open(API_FULL_URL).read().decode())
+	
+	def getBTC(self):
+		return self.__json__["BTC"]["USD"]
+	
+	def getLTC(self):
+		return self.__json__["LTC"]["USD"]
+	
+	def getETH(self):
+		return self.__json__["ETH"]["USD"]
+	
+	def getDOGE(self):
+		return self.__json__["DOGE"]["USD"]
+	
+	def getIOTA(self):
+		return self.__json__["IOTA"]["USD"]
+
+if __name__ == '__main__':
+	data = getLiveData()
+	btc = data.getBTC()
+	ltc = data.getLTC()
+	eth = data.getETH()
+	doge = data.getDOGE()
+	iota = data.getIOTA()
+	print("1 BTC = {}\n1 LTC = {}\n1 ETH = {}\n1 DOGE = {}\n1 IOTA = {}".format(btc,ltc,eth,doge,iota))
