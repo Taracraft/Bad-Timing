866a2864adab6af4ac779682ba933a473b8acf8f
Taracraft <info@bad-timing.eu>
2021-10-15 04:09:10 +0200

    Initial Commit

---
+++ Imageupload/db.php
@@ -0,0 +1,6 @@
+<?
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'mc';
+$DATABASE_PASS = 'X3LyjrmFfL5QLjd9';
+$DATABASE_NAME = 'bt_image_';
+?>
---
+++ Imageupload/upload.php
@@ -0,0 +1,88 @@
+<?
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
+?>
+<?php
+$upload_folder = '/var/www/vhosts/bad-timing.eu/httpdocs/Imageupload/upload/'; //Das Upload-Verzeichnis
+$filename = pathinfo($_FILES['datei']['name'], PATHINFO_FILENAME);
+$extension = strtolower(pathinfo($_FILES['datei']['name'], PATHINFO_EXTENSION));
+ 
+ 
+//Überprüfung der Dateiendung
+$allowed_extensions = array('png', 'jpg', 'jpeg', 'gif');
+if(!in_array($extension, $allowed_extensions)) {
+ die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
+}
+ 
+//Überprüfung der Dateigröße
+$max_size = 500*1024; //500 KB
+if($_FILES['datei']['size'] > $max_size) {
+ die("Bitte keine Dateien größer 500kb hochladen");
+}
+ 
+//Überprüfung dass das Bild keine Fehler enthält
+if(function_exists('exif_imagetype')) { //Die exif_imagetype-Funktion erfordert die exif-Erweiterung auf dem Server
+ $allowed_types = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+ $detected_type = exif_imagetype($_FILES['datei']['tmp_name']);
+ if(!in_array($detected_type, $allowed_types)) {
+ die("Nur der Upload von Bilddateien ist gestattet");
+ }
+}
+ 
+//Pfad zum Upload
+$new_path = $upload_folder.$filename.'.'.$extension;
+ 
+//Neuer Dateiname falls die Datei bereits existiert
+if(file_exists($new_path)) { //Falls Datei existiert, hänge eine Zahl an den Dateinamen
+ $id = 1;
+ do {
+ $new_path = $upload_folder.$filename.'_'.$id.'.'.$extension;
+ $id++;
+ } while(file_exists($new_path));
+}
+ 
+//Alles okay, verschiebe Datei an neuen Pfad
+move_uploaded_file($_FILES['datei']['tmp_name'], $new_path);
+$src = "/home/Lobby_1.17/plugins/Images/images/";  // source folder or file
+$dest = "/var/www/vhosts/bad-timing.eu/httpdocs/Imageupload";   // destination folder or file        
+
+shell_exec("cp -r $src $dest");
+
+echo "<H2>Copy files completed!</H2>"; //output when done
+echo 'Bild erfolgreich hochgeladen: <a href="'.$new_path.'">'.$new_path.'</a>';
+$upload = true;
+if($upload == 'true'){
+ echo '<br></br>';
+ echo '<br><b>Auflistung!:</br></b>';
+ echo '<br></br>';
+ $ordner = "/var/www/vhosts/bad-timing.eu/httpdocs/Imageupload/images";
+ $verzeichnis = opendir($ordner); 
+        while ($file = readdir ($verzeichnis)) 
+        {
+        	
+         if($file != "." && $file != "..") 
+            {
+             if(is_dir($ordner."/".$file)) 
+                {
+                 echo "/".$file."<br/>";
+                    } 
+                     else 
+                    {
+                     // kompletter Pfad
+                     $compl = "https://bad-timing.eu/Imageupload/images"."/".$file;
+                     echo '<br></br>';
+                     echo "<img src=\"".$compl."\" height=\"250\" width=\"250\"></img><br/>";
+                     echo "<center><h1>".$file."</h1></center>";
+                     }
+            }
+        }
+    echo "</select>";
+
+
+}
+ ?>
