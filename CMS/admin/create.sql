fee4b7e6635ba07267f3035082a9537bf1023096
Taracraft <90864484+Taracraft@users.noreply.github.com>
2022-09-01 05:09:51 +0200

    docs

--- CMS/admin/create.sql
+++ CMS/admin/create.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   	`email` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
+ALTER TABLE accounts ADD activation_code varchar(50) DEFAULT ''
 INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa', 'test@test.com');
