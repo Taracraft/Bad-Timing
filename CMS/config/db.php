73a21ec0d0983c0bace4f3c235d717f54acc19b6
Taracraft <90864484+Taracraft@users.noreply.github.com>
2024-08-14 20:04:25 +0200

    Update db.php

--- CMS/config/db.php
+++ CMS/config/db.php
@@ -1,8 +1,8 @@
 <?php
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'cms';
-$DATABASE_PASS = '9hAf9crSQxRz9BZF';
-$DATABASE_NAME = 'cms_';
+$DATABASE_USER = '';
+$DATABASE_PASS = '';
+$DATABASE_NAME = '';
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
